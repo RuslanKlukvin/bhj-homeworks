e30914a822210e694b2a58b6eed3ea7ebe88f73f
password <i-s-istfaka@yandex.ru>
2023-05-28 21:07:03 +0700

    'commit end'

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,7 +1,6 @@
 function showSlide(numberSlide) {
 
   let showSlideNumber = sliderItem.findIndex((number, index, arr) => arr[index].classList.contains('slider__item_active') === true);
-
   sliderItem[showSlideNumber].classList.remove('slider__item_active');
   sliderDot[showSlideNumber].classList.remove('slider__dot_active');
 
@@ -15,27 +14,18 @@ function showSlide(numberSlide) {
 
   sliderItem[numberSlide].classList.add('slider__item_active');
   sliderDot[numberSlide].classList.add('slider__dot_active');
-
 }
 
 function prev() {
-
   let showSlideNumber = sliderItem.findIndex((number, index, arr) => arr[index].classList.contains('slider__item_active') === true);
-
   showSlideNumber--;
-
   showSlide(showSlideNumber);
-
 }
 
 function next() {
-
   let showSlideNumber = sliderItem.findIndex((number, index, arr) => arr[index].classList.contains('slider__item_active') === true);
-
   showSlideNumber++;
-
   showSlide(showSlideNumber)
-
 }
 
 const sliderItem = [...document.querySelectorAll('.slider__item')];
